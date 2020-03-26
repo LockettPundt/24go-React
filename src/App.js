f4780107e68990fb961aa1505db2de326ee9a1f9
Lockett Pundt <kalifornium@hotmail.com>
2020-03-26 16:30:44 -0400

    created the event fetch call and passed each event into an instance of the event component. it renders out a <p>event.title</p> at the moment but it does work.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Event from './components/Events'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      testData: [],
+    }
+  }
+  async componentDidMount() {
+    const test = await this.fetchEvent("music");
+    const data = test.events.event
+    this.setState({
+      testData: data,
+    })
+  }
+  
+  fetchEvent = async (eventType) => {
+    const response = await fetch(`https://api.eventful.com/json/events/search?q=${eventType}&c=${eventType}&t=today&l=
+    30305&within=25&sort_direction=descending&sort_order=popularity&page_size=20&app_key=QfJVpR8FLcNsHKLG`);
+    const json = await response.json();
+    // console.log(json);
+    return json;
+}
+  
+  
+  
+  
+  
+  
+  render() {
+    const { testData } = this.state;
+    const events = testData.map(item => { return <Event event={item}/>})
+    return (
+      <div className="App">
+        {events}
+      </div>
+    );
+    
+  }
 }
 
 export default App;
