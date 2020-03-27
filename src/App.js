c690a21d62935d91dc39a4b7fe7d6e47b090118e
Lockett Pundt <kalifornium@hotmail.com>
2020-03-26 22:11:37 -0400

    fetching comedy and music events and populating the board with them. the dropdowns aren't very functional at the moment.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import './App.css';
 import Event from './components/Events'
@@ -6,22 +7,27 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      testData: [],
+      musicData: [],
+      comedyData: [],
     }
   }
+  
   async componentDidMount() {
-    const test = await this.fetchEvent("music");
-    const data = test.events.event
+    const musicFetch = await this.fetchEvent("music");
+    const musicData = musicFetch.events.event
+    
+    const comedyFetch = await this.fetchEvent("comedy");
+    const comedyData = comedyFetch.events.event
     this.setState({
-      testData: data,
+      comedyData: comedyData || ['Sorry. There are no Comedy Events in your area.'],
+      musicData: musicData || ['Sorry. There are no Music Events in your area.'],
     })
   }
   
   fetchEvent = async (eventType) => {
     const response = await fetch(`https://api.eventful.com/json/events/search?q=${eventType}&c=${eventType}&t=today&l=
-    30305&within=25&sort_direction=descending&sort_order=popularity&page_size=20&app_key=QfJVpR8FLcNsHKLG`);
+    10001&within=25&sort_direction=descending&sort_order=popularity&page_size=20&app_key=QfJVpR8FLcNsHKLG`);
     const json = await response.json();
-    // console.log(json);
     return json;
 }
   
@@ -31,11 +37,51 @@ class App extends Component {
   
   
   render() {
-    const { testData } = this.state;
-    const events = testData.map(item => { return <Event event={item}/>})
+    const { musicData, comedyData } = this.state;
+    const comedyEvents = !!comedyData ? comedyData.map((item, index) => { return <Event key={index} event={item}/>}) : <p>Sorry. There are no Comedy Events in your area.</p>;
+    const musicEvents = musicData.map((item, index) => { return <Event key={index} event={item}/>});
     return (
-      <div className="App">
-        {events}
+      <div classNameName="App">
+        <section className="results" id="dropdownContainer">
+
+        <ul className="vertical menu dropDownMenuTitle" data-accordion-menu
+          data-multi-open="false">
+          <li>
+            <a href="#" className="mainDropdownTitle">Local Events</a>
+            <ul className="menu vertical nested">
+              <li>
+                <a href="#" id="music" className="dropDownMenuTitle">Music</a>
+                <ul className="menu vertical nested" id="musicList">
+                  {musicEvents}
+                </ul>
+              </li>
+              <li>
+                <a href="#" id="comedy" className="dropDownMenuTitle">Comedy</a>
+                <ul className="menu vertical nested" id="comedyList">
+                  {comedyEvents}
+                </ul>
+              </li>
+              <li>
+                <a href="#" id="sports" className="dropDownMenuTitle">Sports</a>
+                <ul className="menu vertical nested" id="sportsList">
+
+                </ul>
+              </li>
+              <li>
+                <a href="#" id="film" className="dropDownMenuTitle">Film</a>
+                <ul className="menu vertical nested" id="filmList">
+
+                </ul>
+              </li>
+              <li>
+                <a href="#" id="culture" className="dropDownMenuTitle">Culture</a>
+                <ul className="menu vertical nested" id="cultureList">
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        </section>
       </div>
     );
     
